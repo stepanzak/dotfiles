c17dfddfbc513a919efbec5b64955b5225217f3d
Štěpán Žák <stepka279.zak@gmail.com>
2023-03-31 19:53:50 +0200

    just another commit lol

---
+++ dot_config/nvim/lua/custom/chadrc.lua
@@ -0,0 +1,26 @@
+---@type ChadrcConfig
+local M = {}
+
+-- Path to overriding theme and highlights files
+local highlights = require "custom.highlights"
+
+M.ui = {
+  theme = "gruvbox",
+  theme_toggle = { "gruvbox", "one_light" },
+
+  hl_override = highlights.override,
+  hl_add = highlights.add,
+
+nvdash = {
+  load_on_startup = true
+  }
+}
+
+M.plugins = "custom.plugins"
+
+-- check core.mappings for table structure
+M.mappings = require "custom.mappings"
+
+return M
+
+
