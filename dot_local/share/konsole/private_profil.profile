bcef3afb5221e577c10fa185138d66ad293c6834
Štěpán Žák <stepka279.zak@gmail.com>
2023-04-11 20:46:50 +0200

    added my new KDE Plasma color scheme and konsole color scheme called Sunset

---
+++ dot_local/share/konsole/private_profil.profile
@@ -0,0 +1,10 @@
+[Appearance]
+ColorScheme=Sunset
+Font=FuraMono Nerd Font,15,-1,5,50,0,0,0,0,0
+
+[General]
+Name=profil
+Parent=FALLBACK/
+
+[Scrolling]
+HistoryMode=2
--- run_install-vscodium-extensions.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-cat vscodium-extensions.txt | while read extension || [[ -n $extension ]];
-do
-  codium --install-extension $extension --force
-done
