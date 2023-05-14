4cd86f561d20863a009f5c537a04adf2fe35cb36
Štěpán Žák <stepka279.zak@gmail.com>
2023-05-14 17:06:01 +0200

    set nvchad themes to gruvbox{,_light}

--- dot_config/nvim/lua/custom/chadrc.lua
+++ dot_config/nvim/lua/custom/chadrc.lua
@@ -6,7 +6,7 @@ local highlights = require "custom.highlights"
 
 M.ui = {
   theme = "gruvbox",
-  theme_toggle = { "gruvbox", "one_light" },
+  theme_toggle = { "gruvbox", "gruvbox_light" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
