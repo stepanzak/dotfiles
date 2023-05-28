e1463b2a5d26e49b008b4691f1b115858ffb709d
Štěpán Žák <stepka279.zak@gmail.com>
2023-05-28 13:36:26 +0200

    set nvim foldmethod option to marker

--- dot_config/nvim/lua/custom/init.lua
+++ dot_config/nvim/lua/custom/init.lua
@@ -1,10 +1,14 @@
--- local autocmd = vim.api.nvim_create_autocmd
+local autocmd = vim.api.nvim_create_autocmd
+local opt = vim.opt
+
+
+opt.foldmethod = "marker"
 
 -- Auto resize panes when resizing nvim window
--- autocmd("VimResized", {
---   pattern = "*",
---   command = "tabdo wincmd =",
--- })
+autocmd("VimResized", {
+  pattern = "*",
+  command = "tabdo wincmd =",
+})
 
 -- require("luasnip").filetype_extend("vue", {"html"})
 -- require("luasnip/loaders/from_vscode").lazy_load()
