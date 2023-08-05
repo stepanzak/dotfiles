b83c4af436a4a2e240899beb3d4ee31a6ec95ae2
Štěpán Žák <stepka279.zak@gmail.com>
2023-08-05 11:31:52 +0200

    nvchad nvim: changed nvdash welcome screen ascii art to nvchad logo

--- dot_config/nvim/lua/custom/chadrc.lua
+++ dot_config/nvim/lua/custom/chadrc.lua
@@ -2,40 +2,53 @@
 local M = {}
 
 -- Path to overriding theme and highlights files
-local highlights = require "custom.highlights"
+local highlights = require("custom.highlights")
 
 M.ui = {
-  theme = "gruvbox",
-  theme_toggle = { "gruvbox", "gruvbox_light" },
+	theme = "gruvbox",
+	theme_toggle = { "gruvbox", "gruvbox_light" },
 
-  hl_override = highlights.override,
-  hl_add = highlights.add,
+	hl_override = highlights.override,
+	hl_add = highlights.add,
 
-  nvdash = {
-    load_on_startup = true,
-    header = {
-      "                 ██████                 ",
-      "             ████▒▒▒▒▒▒████             ",
-      "           ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒██           ",
-      "         ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██         ",
-      "       ██▒▒▒▒▒▒▒▒    ▒▒▒▒▒▒▒▒           ",
-      "       ██▒▒▒▒▒▒  ▒▒▓▓▒▒▒▒▒▒  ▓▓▓▓       ",
-      "       ██▒▒▒▒▒▒  ▒▒▓▓▒▒▒▒▒▒  ▒▒▓▓       ",
-      "     ██▒▒▒▒▒▒▒▒▒▒    ▒▒▒▒▒▒▒▒    ██     ",
-      "     ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██     ",
-      "     ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██     ",
-      "     ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██     ",
-      "     ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██     ",
-      "     ██▒▒██▒▒▒▒▒▒██▒▒▒▒▒▒▒▒██▒▒▒▒██     ",
-      "     ████  ██▒▒██  ██▒▒▒▒██  ██▒▒██     ",
-      "     ██      ██      ████      ████     ",
-    },
-  },
+	nvdash = {
+		load_on_startup = true,
+		-- header = {
+		--   "                 ██████                 ",
+		--   "             ████▒▒▒▒▒▒████             ",
+		--   "           ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒██           ",
+		--   "         ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██         ",
+		--   "       ██▒▒▒▒▒▒▒▒    ▒▒▒▒▒▒▒▒           ",
+		--   "       ██▒▒▒▒▒▒  ▒▒▓▓▒▒▒▒▒▒  ▓▓▓▓       ",
+		--   "       ██▒▒▒▒▒▒  ▒▒▓▓▒▒▒▒▒▒  ▒▒▓▓       ",
+		--   "     ██▒▒▒▒▒▒▒▒▒▒    ▒▒▒▒▒▒▒▒    ██     ",
+		--   "     ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██     ",
+		--   "     ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██     ",
+		--   "     ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██     ",
+		--   "     ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██     ",
+		--   "     ██▒▒██▒▒▒▒▒▒██▒▒▒▒▒▒▒▒██▒▒▒▒██     ",
+		--   "     ████  ██▒▒██  ██▒▒▒▒██  ██▒▒██     ",
+		--   "     ██      ██      ████      ████     ",
+		-- },
+
+		header = {
+			"  NvChad                     ",
+			"     ▄▄         ▄ ▄▄▄▄▄▄▄    ",
+			"   ▄▀███▄     ▄██ █████▀     ",
+			"   ██▄▀███▄   ███            ",
+			"   ███  ▀███▄ ███            ",
+			"   ███    ▀██ ███            ",
+			"   ███      ▀ ███            ",
+			"   ▀██ █████▄▀█▀▄██████▄     ",
+			"     ▀ ▀▀▀▀▀▀▀ ▀▀▀▀▀▀▀▀▀▀    ",
+			"        Powered by Neovim    ",
+		},
+	},
 }
 
 M.plugins = "custom.plugins"
 
 -- check core.mappings for table structure
-M.mappings = require "custom.mappings"
+M.mappings = require("custom.mappings")
 
 return M
