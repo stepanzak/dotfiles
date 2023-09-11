0782a04b963d998990c84b18950266a5ca44999e
Štěpán Žák <stepanzak@proton.me>
2023-09-11 19:03:35 +0200

    added Discord rich presence plugin to Neovim

--- dot_config/nvim/lua/custom/private_plugins.lua
+++ dot_config/nvim/lua/custom/private_plugins.lua
@@ -56,11 +56,31 @@ local plugins = {
   },
 
   -- Install a plugin
+  -- {
+  --   "max397574/better-escape.nvim",
+  --   event = "InsertEnter",
+  --   config = function()
+  --     require("better_escape").setup()
+  --   end,
+  -- },
+
   {
-    "max397574/better-escape.nvim",
-    event = "InsertEnter",
+    "andweeb/presence.nvim",
+    lazy = false,
     config = function()
-      require("better_escape").setup()
+      require("presence").setup {
+        -- General options
+        neovim_image_text = "The Holy Text Editor that's better than Emacs", -- Text displayed when hovered over the Neovim image
+        blacklist = {}, -- A list of strings or Lua patterns that disable Rich Presence if the current file name, path, or workspace matches
+
+        -- Rich Presence text options
+        editing_text = "Breaking %s", -- Format string rendered when an editable file is loaded in the buffer (either string or function(filename: string): string)
+        file_explorer_text = "Lost in %s", -- Format string rendered when browsing a file explorer (either string or function(file_explorer_name: string): string)
+        git_commit_text = "Committing changes (and war crimes)", -- Format string rendered when committing changes in git (either string or function(filename: string): string)
+        plugin_manager_text = "Installing some shiny plugins", -- Format string rendered when managing plugins (either string or function(plugin_manager_name: string): string)
+        reading_text = "Reading through %s", -- Format string rendered when a read-only or unmodifiable file is loaded in the buffer (either string or function(filename: string): string)
+        workspace_text = "Trying to work on %s", -- Format string rendered when in a git repository (either string or function(project_name: string|nil, filename: string): string)
+      }
     end,
   },
 
