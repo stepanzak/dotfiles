f2ed902c332553df36c294274c70e9f5a9050ca4
Štěpán Žák <stepanzak@proton.me>
2023-10-22 18:57:50 +0200

    fixed ls eza alias --icons argument

--- dot_config/zsh/config.d/05-aliases.zsh
+++ dot_config/zsh/config.d/05-aliases.zsh
@@ -5,8 +5,8 @@ alias d='dirs -v'
 for index ({1..9}) alias "$index"="cd +${index}"; unset index
 
 #general aliases
-alias "bls=eza -a --group-directories-first -s modified --icons"
-alias "ls=eza -a --group-directories-first -s name --icons"
+alias "bls=eza -a --group-directories-first -s modified --icons=always"
+alias "ls=eza -a --group-directories-first -s name --icons=always"
 alias "sl=sl -eawd3"
 alias "rt=trash put"
 alias "chz=chezmoi"
