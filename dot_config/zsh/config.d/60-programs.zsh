ac364b2502d9125fa93c63746ce3c9e5833a9b80
Štěpán Žák <stepanzak@proton.me>
2024-04-19 12:59:35 +0200

    added "yy" yazi shell wrapler into zsh config

--- dot_config/zsh/config.d/60-programs.zsh
+++ dot_config/zsh/config.d/60-programs.zsh
@@ -36,3 +36,13 @@ case ":$PATH:" in
   *) export PATH="$PNPM_HOME:$PATH" ;;
 esac
 # pnpm end
+
+# yy shell wrapper that provides the ability to change the current working directory when exiting Yazi.
+function yy() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		cd -- "$cwd"
+	fi
+	rm -f -- "$tmp"
+}
