0b2012b4a16534e4f69921469f97be5ab86a18e5
Štěpán Žák <stepanzak@proton.me>
2024-12-13 19:36:14 +0100

    [zsh] added some quality of life zsh options

--- dot_config/zsh/config.d/10-zsh-config.zsh
+++ dot_config/zsh/config.d/10-zsh-config.zsh
@@ -8,3 +8,7 @@ SAVEHIST=1000
 setopt AUTO_PUSHD # Push the current directory visited on the stack.
 setopt PUSHD_IGNORE_DUPS # Do not store duplicates in the stack.
 setopt PUSHD_SILENT # Do not print the directory stack after pushd or popd.
+setopt AUTO_CD # Cd to a directory without typing cd.
+setopt CORRECT_ALL # Try to correct the spelling of commands and arguments.
+setopt HIST_IGNORE_SPACE # Do not store commands that start with a space in the history.
+setopt INTERACTIVE_COMMENTS # Allow comments in interactive shells.
