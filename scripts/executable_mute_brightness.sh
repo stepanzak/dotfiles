4aa0306525427209d13b1b916af3b6683716158f
Štěpán Žák <stepanzak@proton.me>
2025-04-13 20:59:08 +0200

    [niri] replaced overthinked script with of niri's builtin power-off-monitors

--- scripts/executable_mute_brightness.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-current_brightness=$(brightnessctl get)
-if [ "$current_brightness" -gt 0 ]; then
-	echo "$current_brightness" >/tmp/brightness_before_muted
-	brightnessctl set 0
-else
-	brightnessctl set "$(cat "/tmp/brightness_before_muted")"
-fi
