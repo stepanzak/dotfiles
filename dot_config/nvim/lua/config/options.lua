3416b5c79e30278b0e361bd9cd6dc117aa365895
Štěpán Žák <stepanzak@proton.me>
2025-04-18 18:34:41 +0200

    [yazi][nvim] add neovide options to nvim and yazi keymap to edit file in neovide

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -2,3 +2,24 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.lazyvim_python_lsp = "basedpyright"
+
+-- Neovide config
+vim.g.neovide_scale_factor = 0.8
+vim.g.neovide_hide_mouse_when_typing = true
+
+if vim.g.neovide then
+  vim.keymap.set("v", "<C-S-c>", '"+y') -- Copy
+  vim.keymap.set("c", "<C-S-v>", "<C-R>+") -- Paste command mode
+  vim.keymap.set("i", "<C-S-v>", '<C-o>"+P') -- Paste insert mode
+
+  -- change text scale
+  local change_scale_factor = function(delta)
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+  end
+  vim.keymap.set("n", "<C-=>", function()
+    change_scale_factor(1.1)
+  end)
+  vim.keymap.set("n", "<C-->", function()
+    change_scale_factor(1 / 1.1)
+  end)
+end
